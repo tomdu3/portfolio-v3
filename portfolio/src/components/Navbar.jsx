dad468557549db7af97a40570e5210018402833b
Tomislav Dukez <tomdu3@ymail.com>
2025-04-16 15:29:47 +0100

    fix(navbar): add useEffect import

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
+import { useEffect } from "react";
+
 export const Navbar = ({ navOpen, setNavOpen }) => {
   useEffect(() => {
-    document.body.style.overflow = navOpen ? "hidden" : "auto";
+    document.body.style.overflow = navOpen ? "hidden" : "";
   });
   return (
     <nav className='fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg'>
       <div className='max-w-5xl mx-auto px-4'>
         <div className='flex justify-between items-center h-16'>
           <a href='#home' className='font-mono text-xl font-bold text-white'>
-            {" "}
             TomDcoding<span className='text-blue-400'>.net</span>
           </a>
           <div
