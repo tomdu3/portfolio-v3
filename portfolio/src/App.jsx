50b54d1b553d29b22760720560916b1701a75392
Tomislav Dukez <tomdu3@ymail.com>
2025-04-16 14:51:42 +0100

    feat(navbar): add desktop navbar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from "react";
 import "./App.css";
 import { StartAnimation } from "./components/StartAnimation";
 import './index.css';
+import {Navbar} from "./components/Navbar";
+
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
-
+  const [navOpen, setNavOpen] = useState(false);
+  
   return ( 
     <>
-      {!isLoaded && <StartAnimation onComplete={() => setIsLoaded(true)} />}
+      {!isLoaded && <StartAnimation onComplete={() => setIsLoaded(true)} />} {" "}
+      <div className={`min-h-screen transition-opacity duration-700 ${
+        isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+          <Navbar navOpen={navOpen} setNavOpen={setNavOpen} />
+      </div>
     </>
     );   
 }
