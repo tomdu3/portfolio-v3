730d5234b7d580d2ed496b60c97ce9ebebc75c09
Tomislav Dukez <tomdu3@ymail.com>
2025-04-20 22:03:25 +0100

    fix change colors on h1

--- portfolio/src/components/sections/Home.jsx
+++ portfolio/src/components/sections/Home.jsx
@@ -2,7 +2,7 @@ export const Home = () => {
   return (
     <section id="home" className="min-h-screen flex items-center justify-center relative">
         <div className="text-center z-10 px-4">
-            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-pink-500 to-red-500 bg-clip-text text-transparent leading-right">Hi, I am Tomislav Dukez</h1> 
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-fuchsia-500 to-red-500 bg-clip-text text-transparent leading-right">Hi, I am Tomislav Dukez</h1>   
             <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
                 I am a Full Stack Software Developer, specialized in Predictive Analytics. I am proficient in JavaScript, Python, Bootstrap, Flask, Django, and PostgreSQL, as well as in Data Science tools like NumPy, Pandas, TensorFlow, Scikit-learn, Streamlit, Tableau, and more.
             </p>
