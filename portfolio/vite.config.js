f7e66f085f0960cff996025b0ee443d632c77ae8
Tomislav Dukez <tomdu3@ymail.com>
2025-04-29 08:44:51 +0100

    add server config for vite

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -4,5 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Optional: allows external connections
+    allowedHosts: [
+      'tomdu3.co.uk', // Your domain
+      'localhost',     // Standard localhost
+      '127.0.0.1'     // Local IP
+    ]
+  },
   plugins: [react(), tailwindcss()],
 })
