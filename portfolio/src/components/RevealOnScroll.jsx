75c071cf462cf406089ba65cab5fa88bfc8a63bf
Tomislav Dukez <tomdu3@ymail.com>
2025-04-29 06:41:09 +0100

    fix component dissapears when scrolled away

--- portfolio/src/components/RevealOnScroll.jsx
+++ portfolio/src/components/RevealOnScroll.jsx
@@ -8,25 +8,31 @@ export const RevealOnScroll = ({ children }) => {
             ([entry]) => {
                 if (entry.isIntersecting) {
                     ref.current.classList.add('visible-on-scroll');
+                } else {
+                    ref.current.classList.remove('visible-on-scroll');
                 }
             },
-            {threshold: 0.2, rootMargin: '0px 0px -50px 0px'}
-            );
-
-            if (ref.current) {
-                observer.observe(ref.current);
+            {
+                threshold: 0.2,
+                rootMargin: '0px 0px -50px 0px'
             }
+        );
 
-            return () => {
-                observer.disconnect();
-            };
-        });
+        if (ref.current) {
+            observer.observe(ref.current);
+        }
+
+        return () => {
+            if (ref.current) {
+                observer.unobserve(ref.current);
+            }
+            observer.disconnect();
+        };
+    }, []);
 
     return (
         <div ref={ref} className="show">
-            
             {children}
-        
         </div>
     );
 };
