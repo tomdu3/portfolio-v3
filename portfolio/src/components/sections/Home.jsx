4c9694ba82ad21c28c1753813f19cdc84752a2c1
Tomislav Dukez <tomdu3@ymail.com>
2025-04-29 10:48:01 +0100

    fix contact link mispelled

--- portfolio/src/components/sections/Home.jsx
+++ portfolio/src/components/sections/Home.jsx
@@ -11,7 +11,7 @@ export const Home = () => {
             </p>
             <div className="flex justify-center space-x-4">
                 <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">View Projects</a>
-                <a href="#comtact" className="border border-blue-500 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-300  hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2) hover:bg-blue-500/10]">Contact Me</a>
+                <a href="#contact" className="border border-blue-500 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-300  hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2) hover:bg-blue-500/10]">Contact Me</a>
             </div>
           </div>
         </RevealOnScroll>
