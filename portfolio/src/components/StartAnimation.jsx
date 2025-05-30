e6c5dcf060ad3ff0efeb8b53d66145bfe1af0998
Tomislav Dukez <tomdu3@ymail.com>
2025-05-30 14:06:40 +0100

    fix update colours

--- portfolio/src/components/StartAnimation.jsx
+++ portfolio/src/components/StartAnimation.jsx
@@ -26,7 +26,7 @@ export const StartAnimation = ({ onComplete }) => {
       </div>
 
       <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
-        <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6 animate-loading-bar">
+        <div className="w-[40%] h-full bg-red-500 shadow-[0_0_15px_#f63b70 animate-loading-bar">
           {" "}
         </div>
       </div>
