60089663169e830f9abe913071263f9e64bacfc7
Tomislav Dukez <tomdu3@ymail.com>
2025-07-22 10:27:20 +0100

    update animation change

--- portfolio/src/components/StartAnimation.jsx
+++ portfolio/src/components/StartAnimation.jsx
@@ -1,29 +1,58 @@
-import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 export const StartAnimation = ({ onComplete }) => {
-  const [text, setText] = useState("");
   const fullText = "print('Hello World! Python Forever!')";
+  const letters = Array.from(fullText);
 
-  useEffect(() => {
-    let index = 0;
-    const interval = setInterval(() => {
-      setText(fullText.slice(0, index));
-      index++;
-      if (index > fullText.length) {
-        clearInterval(interval);
-        setTimeout(() => {
-          onComplete();
-        }, 1000);
-      }
-    }, 100);
-    return () => clearInterval(interval);  // to avoid memory leaks
-  }, [onComplete]);
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: (i = 1) => ({
+      opacity: 1,
+      transition: { staggerChildren: 0.05, delayChildren: 0.04 * i },
+    }),
+  };
+
+  const childVariants = {
+    hidden: {
+      opacity: 0,
+      y: 20,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        damping: 12,
+        stiffness: 100,
+      },
+    },
+  };
 
   return (
     <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
-      <div className="mb-4 text-4xl font-mono font-bold text-center">
-        {text} <span className="animate-blink ml-1"> | </span>
-      </div>
+      <motion.div
+        className="mb-4 text-2xl md:text-4xl font-mono font-bold text-center"
+        style={{ display: "flex", overflow: "hidden" }}
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+        onAnimationComplete={() => setTimeout(() => onComplete(), 1000)}
+      >
+        {letters.map((letter, index) => (
+          <motion.span
+            key={index}
+            variants={childVariants}
+            style={{ position: "relative" }}
+          >
+            {letter === " " ? "\u00A0" : letter}
+          </motion.span>
+        ))}
+      </motion.div>
 
       <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
         <div className="w-[40%] h-full bg-red-500 shadow-[0_0_15px_#f63b70 animate-loading-bar">
@@ -31,5 +60,5 @@ export const StartAnimation = ({ onComplete }) => {
         </div>
       </div>
     </div>
-    )
+  );
 };
